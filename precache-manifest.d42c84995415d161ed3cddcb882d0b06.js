af28b72bd5de2cb172ab89197f1c6cc2c7bff8e2
amyshchyshen <korym85@gmail.com>
2020-02-05 22:50:29 +0200

    Updates

---
+++ precache-manifest.d42c84995415d161ed3cddcb882d0b06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609e16c4175e81337de9331d7fe085d4",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "31ffaa4bcb8b17036887",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.dbaacbbb.chunk.css"
+  },
+  {
+    "revision": "9a722f7c0cb968d8b961",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.9e594048.chunk.css"
+  },
+  {
+    "revision": "31ffaa4bcb8b17036887",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a6e2d878.chunk.js"
+  },
+  {
+    "revision": "9a722f7c0cb968d8b961",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.18252b9c.chunk.js"
+  },
+  {
+    "revision": "7cdf5b974de4e0529521",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.88ce524a.js"
+  }
+]);
